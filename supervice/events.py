d30c722078aace6c18a99dae6608190d6a951de3
Farshid Ashouri <farsheed.ashouri@gmail.com>
2026-02-04 20:17:26 +0000

    Add EventType to events

--- supervice/events.py
+++ supervice/events.py
@@ -6,3 +6,16 @@ from typing import Any
 from supervice.logger import get_logger
 MAX_EVENT_QUEUE_SIZE = 1000
 EventHandler = Callable[[Event], Awaitable[None]]
+
+class EventType(Enum):
+    PROCESS_STATE_STARTING = auto()
+    PROCESS_STATE_RUNNING = auto()
+    PROCESS_STATE_BACKOFF = auto()
+    PROCESS_STATE_STOPPING = auto()
+    PROCESS_STATE_EXITED = auto()
+    PROCESS_STATE_STOPPED = auto()
+    PROCESS_STATE_FATAL = auto()
+    PROCESS_STATE_UNKNOWN = auto()
+    PROCESS_STATE_UNHEALTHY = auto()
+    HEALTHCHECK_PASSED = auto()
+    HEALTHCHECK_FAILED = auto()
