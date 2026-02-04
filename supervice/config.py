48c045edc9df78e4793fe118b8b4bd7dd15e0758
Farshid Ashouri <farsheed.ashouri@gmail.com>
2026-02-04 19:03:25 +0000

    Initial config module structure

---
+++ supervice/config.py
@@ -0,0 +1,43 @@
+import configparser
+import os
+from supervice.models import (
+    HealthCheckConfig,
+    HealthCheckType,
+    ProgramConfig,
+    SupervisorConfig,
+)
+VALID_SIGNALS = frozenset(
+    {
+        "HUP",
+        "INT",
+        "QUIT",
+        "ILL",
+        "TRAP",
+        "ABRT",
+        "BUS",
+        "FPE",
+        "KILL",
+        "USR1",
+        "SEGV",
+        "USR2",
+        "PIPE",
+        "ALRM",
+        "TERM",
+        "STKFLT",
+        "CHLD",
+        "CONT",
+        "STOP",
+        "TSTP",
+        "TTIN",
+        "TTOU",
+        "URG",
+        "XCPU",
+        "XFSZ",
+        "VTALRM",
+        "PROF",
+        "WINCH",
+        "IO",
+        "PWR",
+        "SYS",
+    }
+)
