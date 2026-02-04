449504531251f55e1c28158d2a1da3137c34e8db
Farshid Ashouri <farsheed.ashouri@gmail.com>
2026-02-04 20:03:48 +0000

    Initial events module structure

---
+++ supervice/events.py
@@ -0,0 +1,7 @@
+import asyncio
+from collections.abc import Awaitable, Callable
+from dataclasses import dataclass
+from enum import Enum, auto
+from typing import Any
+from supervice.logger import get_logger
+MAX_EVENT_QUEUE_SIZE = 1000
