dc88e04ecb6a69ff8f626e7e92c9134dcdb025a1
Farshid Ashouri <farsheed.ashouri@gmail.com>
2026-02-05 23:22:18 +0000

    Add test_pub_sub to test_core

--- tests/test_core.py
+++ tests/test_core.py
@@ -6,4 +6,26 @@ from supervice.events import Event, EventBus, EventType
 from supervice.rpc import RPCServer
 
 class TestEventBus(unittest.TestCase):
-    pass
+
+    def test_pub_sub(self):
+        async def run():
+            bus = EventBus()
+            bus.start()
+
+            received_events = []
+
+            async def handler(event):
+                received_events.append(event)
+
+            bus.subscribe(EventType.PROCESS_STATE_STARTING, handler)
+
+            event = Event(EventType.PROCESS_STATE_STARTING, {"pid": 123})
+            bus.publish(event)
+
+            await asyncio.sleep(0.1)
+            await bus.stop()
+
+            self.assertEqual(len(received_events), 1)
+            self.assertEqual(received_events[0].type, EventType.PROCESS_STATE_STARTING)
+
+        asyncio.run(run())
