960b992ae947230a69708b23237b96d4c3ad3007
Farshid Ashouri <farsheed.ashouri@gmail.com>
2026-02-05 19:15:33 +0000

    Add MAX_MESSAGE_SIZE to rpc

--- supervice/rpc.py
+++ supervice/rpc.py
@@ -5,3 +5,4 @@ import struct
 from typing import Any
 from supervice.logger import get_logger
 HEADER_SIZE = 4  # 4 bytes for message length (uint32, big-endian)
+MAX_MESSAGE_SIZE = 1024 * 1024  # 1MB max message size
