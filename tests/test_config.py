496aea06fe14cd83893f0978492af6199e5ebce9
Farshid Ashouri <farsheed.ashouri@gmail.com>
2026-02-05 22:03:31 +0000

    Initial test_config module structure

---
+++ tests/test_config.py
@@ -0,0 +1,8 @@
+import os
+import tempfile
+import unittest
+from supervice.config import _parse_bool, _parse_env, parse_config
+from supervice.models import ProgramConfig, SupervisorConfig
+
+class TestModels(unittest.TestCase):
+    pass
