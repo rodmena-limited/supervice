b0deb683fdec9597d709a2f8cb70a8d65fe1db57
Farshid Ashouri <farsheed.ashouri@gmail.com>
2026-02-05 23:07:16 +0000

    Initial test_core module structure

---
+++ tests/test_core.py
@@ -0,0 +1,9 @@
+import asyncio
+import os
+import unittest
+from unittest.mock import AsyncMock, MagicMock
+from supervice.events import Event, EventBus, EventType
+from supervice.rpc import RPCServer
+
+class TestEventBus(unittest.TestCase):
+    pass
